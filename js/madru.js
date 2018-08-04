c111d5724fa2c5aab28e1960ef8c5eff2b21300c
ollieestuff <spiele2002@hotmail.com>
2018-08-03 21:14:55 -0300

    Video slider update Not really quite working yet, but in the process.

--- js/madru.js
+++ js/madru.js
@@ -3,36 +3,40 @@ jQuery(document).ready(function($) {
    $(window).load(function() {
      $('.responsify img').responsify();
    });
-
    $(window).resize(function(){
      $('.responsify img').responsify();
-   })
+  });
 
 
 
    var sliderWidth = $('.slider-scroll').width();
-   var scrollDistance = 0;
 
-   $('.slider-scroll').hammer().on("swipeleft tap press", function(ev) {
-      console.log(ev.type +" gesture detected.");
+   var scrollPosX = 0;
+   var scrollResultX = 0;
 
-      scrollDistance = scrollDistance - sliderWidth;
-      console.log(scrollDistance);
 
-      $('.slider-content').css({
-         "transform" : "translateX(" + scrollDistance + "px)"
+   $('.slider-scroll').hammer().on("panleft", function(ev) {
+      scrollResultX = scrollPosX + ev.gesture.deltaX;
+      console.log('panleft', scrollResultX);
+
+
+      var target = ev.target;
+      $(target).css({
+         'transform': 'translateX(' + scrollResultX + 'px)'
       });
+
    });
-   $('.slider-scroll').hammer().on("swiperight tap press", function(ev) {
-      console.log(ev.type +" gesture detected.");
+   $('.slider-scroll').hammer().on("panright", function(ev) {
 
-      if(scrollDistance != 0) {
 
-         scrollDistance = scrollDistance + sliderWidth;
-         console.log(scrollDistance);
+      if(scrollResultX <= 0) {
 
-         $('.slider-content').css({
-            "transform" : "translateX(" + scrollDistance + "px)"
+         scrollResultX = scrollPosX + ev.gesture.deltaX;
+         console.log('panright', scrollResultX);
+
+         var target = ev.target;
+         $(target).css({
+            'transform': 'translateX(' + scrollResultX + 'px)'
          });
 
       } else {
@@ -40,8 +44,59 @@ jQuery(document).ready(function($) {
          setTimeout(function(){
             $('.slider-content').removeClass('cant-slide-right');
          }, 500);
+
       }
 
    });
+
+   $('.slider-scroll').hammer().on("pan", function(ev) {
+      console.log('Velocidade: ' + ev.gesture.velocity)
+   });
+
+   $('.slider-scroll').hammer().on('panend', function(ev) {
+
+   if( scrollResultX > 0) {
+      scrollResultX = 0;
+
+      $(target).css({
+         'transform': 'translateX(' + scrollResultX + 'px)'
+      });
+   }
+
+
+//-----
+      var velocity = ev.gesture.velocity * 1000;
+
+      var inercia = {drag: scrollResultX};
+
+
+      var target = ev.target;
+
+      $(inercia).animate({
+         drag: scrollResultX + (100 * ev.gesture.velocity),
+      }, {
+         duration: ev.gesture.velocity * 100,
+         step: function(current) {
+            scrollResultX = current;
+            console.log('current: ' + scrollResultX);
+
+
+            $(target).css({
+               'transform': 'translateX(' + current + 'px)'
+            });
+
+
+         }
+      });
+
+
+//----
+
+      scrollPosX = scrollResultX;
+
+      console.log('panend', scrollResultX);
+
+   });
+
 
 });
